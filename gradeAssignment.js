2678f383257d4856adf3e1cfbc28ffaf9d5dbf7e
Jenny273-alt <naungayanjenny90@gmail.com>
2024-10-03 10:06:35 +0700

    gradeAssignment.js

---
+++ gradeAssignment.js
@@ -0,0 +1,21 @@
+let score = prompt("Enter your score:");
+
+// Convert the score to a number
+score = Number(score);
+
+let grade;
+
+if (score >= 90) {
+  grade = "Excellent";
+} else if (score >= 80) {
+  grade = "Good";
+} else if (score >= 70) {
+  grade = "Fair";
+} else {
+  grade = "Needs Improvement";
+}
+
+// Print the grade
+console.log("Your grade is:", grade);
+
+//https://l.facebook.com/l.php?u=https%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2Fdocs%2FWeb%2FJavaScript%2FReference%2FStatements%2Fif...else%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR2M53xrGj3GyT2dyUmJWwCDcliN-4d7qdL1Ra1Upo1YPn8qxDiPK0XVXpg_aem_EuK5FXdWpG0LgdMxGZvFDQ&h=AT2xCctkMWnkFVePGDbdDI68qIKYVqGlYQRrtrM914zDhbthXGeupfHLh-Rv50u2LksU3knMEMIC_0y9RJAqc6MAYnEqvgie8bAQFY4LgXvfGOhJH0Gl-IPDoZ5r69-J8TZMRA
